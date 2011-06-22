8778b58165c133eac9460ba5b33878dfb16b1061
Chris Harper <c_w_harper@yahoo.com>
2011-06-21 22:29:44 -0700

    Finalizing static pages with Help

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,7 +12,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-			:content => "Ruby on Rails Tutorial Sample App | Home")
+                        :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -25,7 +25,8 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
@@ -38,7 +39,22 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | About")
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 end
